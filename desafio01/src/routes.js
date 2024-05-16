2048dbc0644f1ba6ac71634d295937a5dedb6b1a
HugoSalvador <hugossalvador1241@gmail.com>
2024-05-16 14:27:08 -0300

    Estruturando o retorno do método GET

--- desafio01/src/routes.js
+++ desafio01/src/routes.js
@@ -1,21 +1,33 @@
 //Uma task deve ter os seguintes dados
 //id - title - description - completed_at - created_at - updated_at
+import { randomUUID } from 'node:crypto'
+import { Database } from "./database.js"
+
+
+const database = new Database()
 
 export const routes = [
     {
         method:'GET',
         path:'/tasks',
         handler: (req, res) => {
-            const tasks = {
-                id:1,
-                title:'Manutenção no AD',
-                description:'Realizar manutenção no AD',
-                completed_at: '2024-05-11',
-                created_at:'2024-05-02',
-                updated_at:'2024-05-09'
-            }
+            const tasks = database.select('/tasks')
 
             return res.end(JSON.stringify(tasks))
+        },
+    },
+    {
+        method:'POST',
+        path:'/tasks',
+        handler: (req, res) => {
+            const { title, description, completed_at, created_at, updated_at } = req.body
+
+            const task = {
+                id: randomUUID(),
+                title,
+                description,
+                completed_at
+            }
         }
     }
 ]
